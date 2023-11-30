0d3efdf2af83ebf9ba50e13310dc96617e7072c2
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-11-30 17:33:21 +0200

    dark theme and ui fixes

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -9,8 +9,8 @@ const AuthLayout = async ({ children }) => {
   if (session) return redirect("/todo");
 
   return (
-    <div className="relative grid items-center grid-cols-7">
-      <div className="bg-black-main px-6 py-10 w-full min-h-screen max-w-[520px] mx-auto lg:mx-0 lg:max-w-none col-span-full lg:col-span-3 xl:col-span-2 xl:px-12">
+    <div className="bg-black-main relative grid items-center grid-cols-7">
+      <div className="z-10 bg-black-main px-6 py-10 w-full min-h-screen max-w-[520px] mx-auto lg:mx-0 lg:max-w-none col-span-full lg:col-span-3 xl:col-span-2 xl:px-12">
         <Logo className="max-w-fit" />
 
         <div className="relative flex flex-col min-h-[80vh] justify-center h- py-14">
@@ -18,7 +18,7 @@ const AuthLayout = async ({ children }) => {
         </div>
       </div>
 
-      <div className="-z-10 fixed top-0 left-0 right-0 bottom-0 hidden lg:block">
+      <div className="fixed top-0 left-0 right-0 bottom-0 hidden lg:block">
         <div className="max-h-screen">
           <Image
             src="/auth-bg.svg"
