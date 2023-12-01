db0af7b80bec010849ec7bcabb1fc3d4d0e1a726
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-12-01 09:19:20 +0200

    .

--- app/todo/important/page.jsx
+++ app/todo/important/page.jsx
@@ -1,8 +1,41 @@
-export const revalidate = 30
+import { PageHeader } from "@/components";
+import { TasksList } from "@/components/tasks";
+import { nextAuthConfig } from "@/lib/next-auth-config";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+
+export const revalidate = 30;
 
 const ImportantPage = async () => {
+  const session = await getServerSession(nextAuthConfig);
+
+  const tasks = await prisma.tasks.findMany({
+    where: {
+      important: true,
+      project: {
+        uid: session.user.id,
+      },
+    },
+    include: {
+      labels: true,
+    }
+  });
+
   return (
-    <div>ImportantPage</div>
-  )
-}
-export default ImportantPage
+    <>
+      <div className="mb-10 grid gap-4 md:flex md:items-end md:justify-between">
+        <PageHeader
+          type="daily"
+          title={
+            <>
+              <div className="flex gap-3 items-center">Important Tasks</div>
+            </>
+          }
+        />
+      </div>
+
+      <TasksList tasks={tasks} />
+    </>
+  );
+};
+export default ImportantPage;
--- app/todo/page.jsx
+++ app/todo/page.jsx
@@ -1,8 +1,56 @@
-export const revalidate = 30
+import { PageHeader } from "@/components";
+import { TasksList } from "@/components/tasks";
+import { nextAuthConfig } from "@/lib/next-auth-config";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+
+export const revalidate = 30;
 
 const page = async () => {
+  const today = new Date();
+  const startOfToday = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate()
+  );
+  const endOfToday = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 1
+  );
+  
+  const session = await getServerSession(nextAuthConfig);
+
+  const tasks = await prisma.tasks.findMany({
+    where: {
+      dueDate: {
+        gte: startOfToday,
+        lt: endOfToday,
+      },
+      project: {
+        uid: session.user.id,
+      },
+    },
+    include: {
+      labels: true,
+    }
+  });
+
   return (
-    <div>page</div>
-  )
-}
-export default page
+    <>
+      <div className="mb-10 grid gap-4 md:flex md:items-end md:justify-between">
+        <PageHeader
+          type="daily"
+          title={
+            <>
+              <div className="flex gap-3 items-center">My Day</div>
+            </>
+          }
+        />
+      </div>
+
+      <TasksList tasks={tasks} />
+    </>
+  );
+};
+export default page;
