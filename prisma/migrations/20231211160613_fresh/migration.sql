05f288045048e455223be636e00ee0908bf4da4f
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-12-11 18:08:55 +0200

    fix: client regex, yup-schema and db fields length

---
+++ prisma/migrations/20231211160613_fresh/migration.sql
@@ -0,0 +1,86 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(20) NOT NULL,
+    `lastName` VARCHAR(20) NOT NULL,
+    `email` VARCHAR(60) NOT NULL,
+    `hashPassword` VARCHAR(255) NOT NULL,
+    `updated_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    UNIQUE INDEX `email`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `labels` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `uid` INTEGER NOT NULL,
+    `name` VARCHAR(20) NOT NULL,
+    `updated_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `uid`(`uid`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `projects` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `uid` INTEGER NOT NULL,
+    `name` VARCHAR(20) NOT NULL,
+    `color` VARCHAR(9) NULL DEFAULT '#b8255f',
+    `updated_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `uid`(`uid`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `tasks` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `description` TINYTEXT NOT NULL DEFAULT '',
+    `completed` BOOLEAN NOT NULL DEFAULT false,
+    `important` BOOLEAN NOT NULL DEFAULT false,
+    `dueDate` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `uid` INTEGER NULL,
+    `pid` INTEGER NULL,
+
+    INDEX `uid`(`uid`),
+    INDEX `pid`(`pid`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `taskToLabel` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `taskId` INTEGER NOT NULL,
+    `labelId` INTEGER NOT NULL,
+
+    INDEX `taskId`(`taskId`),
+    INDEX `labelId`(`labelId`),
+    UNIQUE INDEX `taskToLabel_taskId_labelId_key`(`taskId`, `labelId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `labels` ADD CONSTRAINT `labels_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- AddForeignKey
+ALTER TABLE `projects` ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- AddForeignKey
+ALTER TABLE `tasks` ADD CONSTRAINT `tasks_uid_fkey` FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `tasks` ADD CONSTRAINT `tasks_pid_fkey` FOREIGN KEY (`pid`) REFERENCES `projects`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `taskToLabel` ADD CONSTRAINT `taskToLabel_taskId_fkey` FOREIGN KEY (`taskId`) REFERENCES `tasks`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `taskToLabel` ADD CONSTRAINT `taskToLabel_labelId_fkey` FOREIGN KEY (`labelId`) REFERENCES `labels`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
